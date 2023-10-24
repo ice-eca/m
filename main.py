87e5dd0c4b7d20d709753e95efe10f448992d768
ice-eca <ice_eca@mail.ru>
2023-10-24 12:28:36 +0500

    bug fix

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def enter_district(message):
     
     markup.add(itembtn1, itembtn2)
     bot.send_message(message.chat.id, 'Школа программирования для детей KIBERone Березовский приветствует вас!\U0001F60A\n \nНа этой неделе мы проводим бесплатный мастер-класс по программированию для детей 7-14 лет\U0001F4BB\n \n\U00002705Ваш ребенок создаст свой первый мультфильм и запрограммирует своего героя в игре Майнкрафт\U0001F5A5\n \n\U00002705Расскажем, как избавить ребенка от игромании и научить компьютерной грамотности, чтобы подготовить к успешному будущему\n \n\U00002705Длительность занятия 60 минут. Все необходимое предоставим. Ничего брать с собой не нужно.\n \nВыберите удобную для обучения локацию\U0001F447' , reply_markup=markup)
+    bot.send_photo(message.chat.id, 'kiber1.png',reply_markup=markup)
 
 def enter_age(message):
     markup = types.InlineKeyboardMarkup()
@@ -56,7 +57,7 @@ def handle_text(message):
 
 def check_and_send(message):
     if district_regex.match(data[message.chat.id]['district']) and age_regex.match(data[message.chat.id]['age']):
-        bot.send_message(message.chat.id, 'Спасибо! Скоро с вами свяжется наш администратор, отправит вам расписание мастер-классов на ближайшую неделю и согласует точное время')
+        bot.send_message(message.chat.id, 'Спасибо! Скоро с вами свяжется наш администратор, отправит вам расписание мастер-классов на ближайшую неделю и согласует точное время\n \nДо встречи на уроке!\U0001F60A')
         bot.send_message(request_chat_id, 'Адрес ' + data[message.chat.id]['district']+' возраст '+data[message.chat.id]['age']+' '+data[message.chat.id]['phone_number'])
         clear_data(message)
     else:
