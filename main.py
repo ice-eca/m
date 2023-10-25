d2e4891d29d27bfd30d35dc50ac6888ec9e89855
ice-eca <ice_eca@mail.ru>
2023-10-25 17:55:31 +0500

    bug fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import telebot
 from telebot import types
 import re
 
-TOKEN = '6731928104:AAF9_eQC2_Mt5ugxvBUz-Ehv1NUrtVsTKD4'
+TOKEN = '6447996107:AAFMcuJPDWTOi2zUFdA_hNOpUGh-dV5a4wQ'
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -10,32 +10,21 @@ phone_number_regex = re.compile(r'^(\+7|8)\d{10}$')
 age_regex = re.compile(r'^\d.*')
 district_regex = re.compile(r'^\D.*')
 data = {}
-request_chat_id = '-4007021404'
+request_chat_id = '-4023403322'
 
 @bot.message_handler(commands=['start'])
-
 def enter_district(message):
     clear_data(message)
-    data[message.chat.id] = {'stage':0}
+    data[message.chat.id] = {'stage':1}
     markup = types.InlineKeyboardMarkup()
-    itembtn1 = types.InlineKeyboardButton(text='Вторник', callback_data='Вторник')
-    itembtn2 = types.InlineKeyboardButton(text='Суббота', callback_data='Суббота')
-    
-    markup.add(itembtn1, itembtn2)
-    bot.send_photo(message.chat.id, open('kiber1.png', 'rb'))
-    bot.send_message(message.chat.id, 'Школа программирования для детей KIBERone в Косулино приветствует вас!\U0001F60A\n \nНа этой неделе мы проводим бесплатный мастер-класс по программированию для детей 7-14 лет\U0001F4BB\n \n\U00002705Ваш ребенок создаст свой первый мультфильм и запрограммирует своего героя в игре Майнкрафт\U0001F5A5\n \n\U00002705Расскажем, как избавить ребенка от игромании и научить компьютерной грамотности, чтобы подготовить к успешному будущему\n \n\U00002705Длительность занятия 60 минут. Все необходимое предоставим. Ничего брать с собой не нужно.\n \nЗанятия будут проходить по адресу: Косулино, ул. Кузнечная, 7A\n \nВыберите удобный день для обучения \U0001F447' , reply_markup=markup)
-    
-def enter_age(message):
-    markup = types.InlineKeyboardMarkup()
-    itembtn1 = types.InlineKeyboardButton(text='7-8 лет', callback_data='7-8')
+    itembtn1 = types.InlineKeyboardButton(text='6-8 лет', callback_data='6-8')
     itembtn2 = types.InlineKeyboardButton(text='9-11 лет', callback_data='9-11')
     itembtn3 = types.InlineKeyboardButton(text='12-14 лет', callback_data='12-14')
     markup.add(itembtn1, itembtn2, itembtn3)
-    bot.send_message(message.chat.id, 'Пожалуйста, укажите возраст вашего ребенка\U0001F447',reply_markup=markup)
-
+    bot.send_message(message.chat.id, 'Пожалуйста, укажите возраст вашего ребенка',reply_markup=markup)
 
 def enter_phone_number(message):
-    bot.send_message(message.chat.id, 'Спасибо! Остался последний шаг\U0001F60A\n \nПожалуйста, введите номер телефона, по которому мы можем с Вами связаться\U0001F4F1')
+    bot.send_message(message.chat.id, 'Пожалуйста, введите номер телефона, по которому мы можем с Вами связаться')
     
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
@@ -57,9 +46,9 @@ def handle_text(message):
         return
 
 def check_and_send(message):
-    if district_regex.match(data[message.chat.id]['district']) and age_regex.match(data[message.chat.id]['age']):
-        bot.send_message(message.chat.id, 'Спасибо! Скоро с вами свяжется наш администратор, отправит вам расписание мастер-классов на ближайшую неделю и согласует точное время\n \nДо встречи на уроке!\U0001F60A')
-        bot.send_message(request_chat_id, 'Желаемый день: ' + data[message.chat.id]['district']+' возраст: '+data[message.chat.id]['age']+' '+data[message.chat.id]['phone_number'])
+    if age_regex.match(data[message.chat.id]['age']):
+        bot.send_message(message.chat.id, 'Спасибо! Скоро с вами свяжется администратор!')
+        bot.send_message(request_chat_id,'Возраст '+data[message.chat.id]['age']+' '+data[message.chat.id]['phone_number'])
         clear_data(message)
     else:
         bot.send_message(message.chat.id, 'Неправильно сформированы ответы на вопросы, поробуйте еще раз')
@@ -72,11 +61,7 @@ def clear_data(message):
 @bot.callback_query_handler(func=lambda call: True)
 def answering(call):
     if call.message.chat.id in data:
-        if data[call.message.chat.id]['stage'] == 0:
-            data[call.message.chat.id]['district'] = call.data
-            data[call.message.chat.id]['stage'] = 1
-            enter_age(call.message)
-        elif data[call.message.chat.id]['stage'] == 1:
+        if data[call.message.chat.id]['stage'] == 1:
             data[call.message.chat.id]['age'] = call.data
             data[call.message.chat.id]['stage'] = 2
             enter_phone_number(call.message)
